1ffedaac6f627bbe5a603490b9d1db7af5741d1b
ipetropolsky <i.petropolsky@hh.ru>
2023-01-04 00:47:30 +0100

    Add map to visualize searching

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
         node: true,
         es2022: true,
     },
+    parserOptions: {
+        sourceType: 'module',
+    },
     rules: {
         'prettier/prettier': 'error',
         'accessor-pairs': 'off',
