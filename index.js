611ec99f0ee317b2ade70346dac85c9390fb9f78
ipetropolsky <i.petropolsky@hh.ru>
2023-01-06 23:29:43 +0100

    handle-errors Add more comments and examples

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-async function getData(url) {
+// Загрузка данных через await
+async function getDataAsync(url) {
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     const response = await fetch(url, {
         method: 'GET',
@@ -7,17 +8,86 @@ async function getData(url) {
         },
         redirect: 'follow',
     });
+
+    // При сетевой ошибке (мы оффлайн) из `fetch` вылетит эксцепшн.
+    // Тут мы даём ему просто вылететь из функции дальше наверх.
+    // Если же его нужно обработать, придётся обернуть в `try` и сам `fetch`:
+    //
+    // try {
+    //     response = await fetch(url, {...});
+    // } catch (error) {
+    //     // Что-то делаем
+    //     throw error;
+    // }
+
+    // Если мы тут, значит, запрос выполнился.
+    // Но там может быть 404, 500, и т.д., поэтому проверяем ответ.
     if (response.ok) {
         return response.json();
     }
-    throw response;
+
+    // Пример кастомной ошибки (если нужно проставить какие-то поля
+    // для внешнего кода). Можно выкинуть и сам `response`, смотря
+    // какой у вас контракт. Главное перевести код в ветку `catch`.
+    const error = {
+        status: response.status,
+        customError: 'wtfAsync',
+    };
+    throw error;
 }
+
+// Загрузка данных через промисы (то же самое что `getDataAsync`)
+function getDataPromise(url) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+    }).then(
+        (response) => {
+            // Если мы тут, значит, запрос выполнился.
+            // Но там может быть 404, 500, и т.д., поэтому проверяем ответ.
+            if (response.ok) {
+                return response.json();
+            }
+            // Пример кастомной ошибки (если нужно проставить какие-то поля
+            // для внешнего кода). Можно зареджектить и сам `response`, смотря
+            // какой у вас контракт. Главное перевести код в ветку `catch`.
+            return Promise.reject({
+                status: response.status,
+                customError: 'wtfPromise',
+            });
+        },
+
+        // При сетевой ошибке (мы оффлайн) из fetch вылетит эксцепшн,
+        // и мы попадём в `onRejected` или в `.catch()` на промисе.
+        // Если не добавить `onRejected` или `catch`, при ошибке будет
+        // эксцепшн `Uncaught (in promise)`.
+        (error) => {
+            // Если не вернуть `Promise.reject()`, для внешнего кода
+            // промис будет зарезолвлен с `undefined`, и мы не попадём
+            // в ветку `catch` для обработки ошибок, а скорее всего
+            // получим другой эксцепшн, потому что у нас `undefined`
+            // вместо данных, с которыми мы работаем.
+            return Promise.reject(error);
+        }
+    );
+}
+
+// Две функции просто для примера, выберите с await или promise, какая нравится
+const getData = getDataAsync || getDataPromise;
 
 async function loadCountriesData() {
     let countries = [];
     try {
+        // ПРОВЕРКА ОШИБКИ №1: ломаем этот урл, заменяя all на allolo,
+        // получаем кастомную ошибку.
         countries = await getData('https://restcountries.com/v3.1/all?fields=name&fields=cca3&fields=area');
     } catch (error) {
+        // console.log('catch for getData');
+        // console.error(error);
         throw error;
     }
     return countries.reduce((result, country) => {
@@ -41,8 +111,12 @@ const output = document.getElementById('output');
     output.textContent = 'Loading…';
     let countriesData = {};
     try {
+        // ПРОВЕРКА ОШИБКИ №2: Ставим тут брейкпоинт и, когда дойдёт
+        // до него, переходим в оффлайн-режим. Получаем эксцепшн из `fetch`.
         countriesData = await loadCountriesData();
     } catch (error) {
+        // console.log('catch for loadCountriesData');
+        // console.error(error);
         output.textContent = 'Something went wrong. Try to reset your compluter.';
         return;
     }
